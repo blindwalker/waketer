5a9faf7a810fa17d253fd5704b04612b6237d812
blindwalker <martinkropf08@gmail.com>
2014-12-16 21:08:30 +0100

    added round music view added band/track gridview

--- app/src/main/java/at/kropf/waketer/fragments/AddBandFragment.java
+++ app/src/main/java/at/kropf/waketer/fragments/AddBandFragment.java
@@ -15,7 +15,7 @@ public class AddBandFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.activity_add_song, container, false);
+        View rootView = inflater.inflate(R.layout.activity_music_overview, container, false);
         return rootView;
     }
 }
